efa9484088d6540427ad9fff1e848b32eedeaa3e
renersullivan <renersullivan1@gmail.com>
2022-01-11 21:31:24 -0300

    subindo class menu

---
+++ menu.py
@@ -0,0 +1,13 @@
+class Menu:
+
+  def  mostrar():
+        escolhaMenu = input('''
+        # 1 - Consultar Saldo
+        # 2 - Depositar Dinheiro
+        # 3 - Fazer um pix
+        # 4 - Sacar dinheiro
+        # 5 - Sair do caixa eletronico
+    ''')
+        return escolhaMenu
+
+       
