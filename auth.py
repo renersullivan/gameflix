3be724e37e63431f50c593f42baf7299ab2a3e25
Felipe Ferreira Cabo <felipe@MacBook-Pro-de-Felipe.local>
2022-01-13 19:07:52 -0300

    Removi while desnecessário

--- auth.py
+++ auth.py
@@ -7,16 +7,12 @@ class Entrar:
         LOGIN_SQL = 'rener'
         SENHA_SQL = '1234'
 
-        login = ''
-        senha = ''
-
-        while (login != LOGIN_SQL and senha != SENHA_SQL): 
-            login = input('Digite seu login: ')
-            senha = input('Digite sua senha: ')
-            if login == LOGIN_SQL and senha == SENHA_SQL:
-                print('Logou com sucesso')
-            else:
-                print('Login ou Senha nao encontrado, tente novamente')
-                return Entrar.login()
+        login = input('Digite seu login: ')
+        senha = input('Digite sua senha: ')
+        if login == LOGIN_SQL and senha == SENHA_SQL:
+            print('Logou com sucesso')
+        else:
+            print('Login ou Senha nao encontrado, tente novamente')
+            return Entrar.login()
        
     
